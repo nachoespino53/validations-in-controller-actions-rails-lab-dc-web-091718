5cd9bdc8b207cfecb5d43e3a2866c7a3b4eab34e
nachoespino53 <nachoespino53@gmail.com>
2018-10-15 13:17:47 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,22 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def update
-    @post.update(post_params)
+  def create
+    @post = Post.new(post_params)
 
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 100}
+
+# validates :title, presence: true
+# validates :content, length: { minimum: 100 }
+# validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
